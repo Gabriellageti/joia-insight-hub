c902b91d198e3733a2c2e02d2cd579cd309b9b00
Gabriellageti <116768796+Gabriellageti@users.noreply.github.com>
2026-07-20 10:57:20 -0300

    Persist UTF-8 consulting workspace: src/components/plano-acao/index.ts

---
+++ src/components/plano-acao/index.ts
@@ -0,0 +1,7 @@
+
+export { TaskCard } from "./TaskCard";
+export { TaskComments } from "./TaskComments";
+export { TaskFilters } from "./TaskFilters";
+export { ConsultingDayWorkspace } from "./ConsultingDayWorkspace";
+export { NextStepsSuggestionModal } from "./NextStepsSuggestionModal";
+export type { SuggestedNextStep } from "./NextStepsSuggestionModal";
