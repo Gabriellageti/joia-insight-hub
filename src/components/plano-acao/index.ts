ccb3454e00e87537f224bafcc0f6dc9399a47ce6
Gabriellageti <116768796+Gabriellageti@users.noreply.github.com>
2026-07-20 10:57:19 -0300

    Prepare UTF-8 consulting workspace: src/components/plano-acao/index.ts

--- src/components/plano-acao/index.ts
+++
@@ -1,5 +0,0 @@
-export { TaskCard } from "./TaskCard";
-export { TaskComments } from "./TaskComments";
-export { TaskFilters } from "./TaskFilters";
-export { NextStepsSuggestionModal } from "./NextStepsSuggestionModal";
-export type { SuggestedNextStep } from "./NextStepsSuggestionModal";
